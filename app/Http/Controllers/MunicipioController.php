35048defaaabb86e10c3b2a3d85188001dbe6897
Jerson Villatoro <201442819@ingenieria.usac.edu.gt>
2018-12-16 23:16:41 -0600

    Servicos de Departamento y Municipio

---
+++ app/Http/Controllers/MunicipioController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Municipio;
+use Illuminate\Support\Facades\DB;
+
+class MunicipioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * 
+     */
+    public function getMunicipios($param){
+        $request->validate([
+            'idDepartamento' => 'required|exists:DEPARTAMENTO,id_dpto'
+        ]);
+
+        $dptos = DB::table('MUNICIPIO')
+            ->select('id_muni as idMunicipio', 'nombre as municipio')
+            ->where('id_dpto','=',$request->get('idDepartamento'))
+            ->get();
+        $d = new Municipio;
+        $d->mensaje = "
+        Lista de municipios recuperada con exito";
+        $d->codigoMensaje = '1';
+        $d->Municipios = $dptos;
+        return response()->json($d);
+    }
+
+    public function getMuni($valor){
+        $json_recibido = json_decode($valor,true);
+        $id_dpto = $json_recibido['idDepartamento'];
+        $d = new Municipio;
+
+        $existe = DB::table('DEPARTAMENTO')
+            ->select('id_dpto')
+            ->where('id_dpto','=',$id_dpto)
+            ->get();
+
+        if($existe == "[]"){
+            $d = new Municipio;
+            $d->mensaje = "No hay Departamento con ese codigo";
+            $d->codigoMensaje = "-1";
+            $d->Municipios = [];
+            return response()->json($d);
+        }
+
+        $dptos = DB::table('MUNICIPIO')
+            ->select('id_muni as idMunicipio', 'nombre as municipio')
+            ->where('id_dpto','=',$id_dpto)
+            ->get();
+
+        $d->mensaje = "Lista de municipios recuperada con exito";
+        $d->codigoMensaje = '1';
+        $d->Municipios = $dptos;
+        return response()->json($d);
+    }
+}
