c48ccc2f3621c852707d1efb4783c8e91884d0a4
KEEC4991 <42172933+KEEC4991@users.noreply.github.com>
2018-12-26 14:23:46 -0600

    cambios vergas

--- resources/views/nacimientos.blade.php
+++ resources/views/nacimientos.blade.php
@@ -1,22 +1,32 @@
 <html>
+    <header>
+        
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    </header>
+
+    
     <body>
         <h1>IMPRESION DE ACTA DE NACIMIENTO</h1>
         <div class="login-page">
-        <div class="form">
-            <form class="register-form">
-            <input type="text" placeholder="name"/>
-            <input type="password" placeholder="password"/>
-            <input type="text" placeholder="email address"/>
-            <button>create</button>
-            <p class="message">Already registered? <a href="#">Sign In</a></p>
-            </form>
-            <form class="login-form">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button>login</button>
-            <p class="message">Not registered? <a href="#">Create an account</a></p>
-            </form>
-        </div>
+            <div class="form">
+                <form class="register-form">
+                <input type="text" placeholder="name"/>
+                <input type="password" placeholder="password"/>
+                <input type="text" placeholder="email address"/>
+                <button>create</button>
+                <p class="message">Already registered? <a href="#">Sign In</a></p>
+                </form>
+                <form class="login-form">
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password"/>
+                <button>login</button>
+                <p class="message">Not registered? <a href="#">Create an account</a></p>
+                </form>
+            </div>
         </div>
     </body>
 </html>
