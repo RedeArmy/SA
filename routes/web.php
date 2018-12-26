3f7139ac2967d822f2b47c0309b3c21df729c455
Jerson Villatoro <201442819@ingenieria.usac.edu.gt>
2018-12-26 15:30:15 -0600

    Prueba de consultar matrimonios

--- routes/web.php
+++ routes/web.php
@@ -29,24 +29,21 @@ Route::resource('servicionacimiento','ServNacimientoController');
 
 Route::get('api/v1/nacs','ServNacimientoController@getPrueba');
 
+//SERVICIOS DE NACIMIENTO
 Route::get('api/v1/imprimirNacimiento/{valor}',"ServNacimientoController@imprimirNacimiento");
-
 Route::get('api/v1/registrarNacimiento/{valor}',"ServNacimientoController@registrarNacimiento");
 
 
 /* Servicios de Departamento y Municipio */
 Route::resource('departamento','DepartamentoController');
-
 Route::get('api/v1/dptos','DepartamentoController@getDptos');
-
 Route::get('api/v1/muni/{valor}','MunicipioController@getMuni');
-
 Route::get('api/v1/dpi_consulta/{valor}','DpiController@ConsultarDpi');
 
+//SERVICIOS DE MATRIMONIO
 Route::get('api/v1/reg_matri/{valor}','MatrimonioController@registrarMatrimonio');
-
 Route::get('api/v1/consul_matri/{valor}','MatrimonioController@consultarMatrimonio');
 
+//SERVICIOS DE DEFUNCION
 Route::get('api/defuncion/registro_defuncion/{valor}','DefuncioneController@registrarDefuncion');
-
 Route::get('api/defuncion/imprimir_defuncion/{valor}','DefuncioneController@imprimirDefuncion');
