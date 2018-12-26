2580d9b002dfd7c5c05ce49000aa0f3e7ddda2fc
KEEC4991 <42172933+KEEC4991@users.noreply.github.com>
2018-12-26 00:52:15 -0600

    Actualizacion de controlador de nacimientos

--- app/Http/Controllers/ServNacimientoController.php
+++ app/Http/Controllers/ServNacimientoController.php
@@ -321,9 +321,11 @@ class ServNacimientoController extends Controller
 
             $valor_depto =$id_departamento[0]['id_dpto']; 
             echo "<BR> VALOR DEPTO: ".$valor_depto."<BR>";
+            $valor_depto = $valor_depto * 1;
             if((log($valor_depto, 10) + 1) == 1){
                 $valor_depto = "0".$id_departamento[0]['id_dpto'];
             }
+
             echo "<BR> VALOR DEPTO despues : ".$valor_depto."<BR>";
 
             $cui_final_generado = $cui_generado.$valor_depto.$municipio;
