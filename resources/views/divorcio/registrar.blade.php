82682d65fc3aad84365617eeda816334cf0c3c8d
Jerson Villatoro <201442819@ingenieria.usac.edu.gt>
2018-12-27 10:54:42 -0600

    Vista de registro de divorcios

---
+++ resources/views/divorcio/registrar.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.admin')
+	@section('content')
+	{!!Form::open(['route'=>'matrimonio.store', 'method'=>'POST'])!!}
+	<div class="form-group">
+		{!!Form::label('hombre','CUI Hombre:')!!}
+		{!!Form::text('cuiHombre',null,['class'=>'form-control','placeholder'=>'Ingresa el CUI del usuario'])!!}
+	</div>
+	<div class="form-group">
+		{!!Form::label('mujer','CUI Mujer:')!!}
+		{!!Form::text('cuiMujer',null,['class'=>'form-control','placeholder'=>'Ingresa el CUI del usuario'])!!}
+	</div>
+	<div class="form-group">
+		{!!Form::label('muni','Municipio:')!!}
+		{!!Form::text('municipio',null,['class'=>'form-control','placeholder'=>'Ingresa el municipio de divorcio'])!!}
+	</div>
+	<div class="form-group">
+		{!!Form::label('lugar','Lugar de Divorcio:')!!}
+		{!!Form::text('lugarDivorcio',null,['class'=>'form-control','placeholder'=>'Ingresa el lugar del divorcio'])!!}
+	</div>
+	<div class="form-group">
+		{!!Form::label('fecha','Fecha de Divorcio:')!!}
+		{!!Form::date('fechaDivorcio',null,['class'=>'form-control','placeholder'=>'Ingresa la fecha del divorcio'])!!}
+	</div>
+	{!!Form::submit('Registrar Divorcio',['class'=>'btn btn-primary'])!!}
+	{!!Form::close()!!}
+	@endsection
