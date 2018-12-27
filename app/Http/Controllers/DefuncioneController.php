301438e9875dc5430992f467c3917c1f7996489c
GaryGiron <gsteph393@gmail.com>
2018-12-27 13:41:19 -0600

    cabios vergas

--- app/Http/Controllers/DefuncioneController.php
+++ app/Http/Controllers/DefuncioneController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Session;
+use Redirect;
 
 class DefuncioneController extends Controller
 {
