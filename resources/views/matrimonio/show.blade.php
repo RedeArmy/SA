2014c0537652beaeff487f0a86ca004a6166b16b
GaryGiron <gsteph393@gmail.com>
2018-12-27 11:32:47 -0600

    iprimir datos

--- resources/views/matrimonio/show.blade.php
+++ resources/views/matrimonio/show.blade.php
@@ -7,12 +7,10 @@
 			<th>Mensaje</th>
 			<th>Data</th>
 		</thead>
-		@foreach($imprimir as $imp)
 			<tbody>
 				<td>{{$imp['status']}}</td>
 				<td>{{$imp['mensaje']}}</td>
 				<td>{{$imp['data']}}</td>
 			</tbody>
-		@endforeach
 	</table>
 	@endsection
