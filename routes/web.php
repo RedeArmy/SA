4c34924aef7a10daf745b189d5185186bbf9d613
Jerson Villatoro <201442819@ingenieria.usac.edu.gt>
2019-01-07 03:48:00 -0600

    adios

--- routes/web.php
+++ routes/web.php
@@ -82,6 +82,8 @@ Route::post('/divorcio/imprimir','DivorcioController@Imprimir');
 
 Route::post('/municipio/lista','MunicipioController@ListaMunis');
 Route::post('/departamento/lista','DepartamentoController@ListaDptos');
+Route::get('/departamento/lista','DepartamentoController@ListaDptos');
+
 
 Route::post('/dpi/consultar','DpiController@Consultar');
 Route::post('/dpi/actualizar','DpiController@Actualizar');
