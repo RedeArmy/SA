96fcdf8eb126ca07709dbffd804be773725faec8
GaryGiron <gsteph393@gmail.com>
2019-01-07 23:08:46 -0600

    cambios en las vistas

--- resources/views/defuncion/resultado.blade.php
+++ resources/views/defuncion/resultado.blade.php
@@ -3,7 +3,7 @@
     <h1>Datos del fallecido</h1>
     <div class="panel panel-default">
         <div class="panel-body">
-            <b>cui: </b>{{$info['cui']]}}
+            <b>cui: </b>{{$info['cui']}}
         </div>
     </div>
     <div class="panel panel-default">
