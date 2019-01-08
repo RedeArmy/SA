5f6c70bf4d22b011c0d73e41def9b6422304cd8c
GaryGiron <gsteph393@gmail.com>
2019-01-07 22:20:00 -0600

    cambios en las vistas

--- resources/views/defuncion/resultado.blade.php
+++ resources/views/defuncion/resultado.blade.php
@@ -3,7 +3,7 @@
     <h1>Datos del fallecido</h1>
     <div class="panel panel-default">
         <div class="panel-body">
-            <b>dfsf: </b>{{$respData}}            
+            <b>dfsf: </b>{{$response}}            
         </div>
     </div>
 </div>
