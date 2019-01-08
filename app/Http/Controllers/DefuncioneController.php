61cfb0cb80711d553522486f1470363a102eee4b
GaryGiron <gsteph393@gmail.com>
2019-01-07 23:04:20 -0600

    cambios en las vistas

--- app/Http/Controllers/DefuncioneController.php
+++ app/Http/Controllers/DefuncioneController.php
@@ -64,7 +64,8 @@ class DefuncioneController extends Controller
                 return view('defuncion.error', compact('err'));
             } else {
                 $respData = json_decode($response, true);
-                $info = json_decode($respData['data'], true);
+
+                $info = $respData['data'];
                 return view('defuncion.resultado',compact('info'));
             }
                         
