635c3106a37a1f3426fa8f1a7f006a54cf70ccfd
KEEC4991 <42172933+KEEC4991@users.noreply.github.com>
2019-01-08 12:14:52 -0600

    Cambios en las vistas de defuncion

--- app/Http/Controllers/ServNacimientoController.php
+++ app/Http/Controllers/ServNacimientoController.php
@@ -90,7 +90,7 @@ class ServNacimientoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Request $request)
+    public function edit()
     {
 
     }
