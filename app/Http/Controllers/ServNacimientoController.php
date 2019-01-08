6d354c8d05677a4219ae7a4c9b1eafaa910fc267
KEEC4991 <42172933+KEEC4991@users.noreply.github.com>
2019-01-07 22:45:56 -0600

    Controladores y rutas de servicios para modificacion de fechas y formatos

--- app/Http/Controllers/ServNacimientoController.php
+++ app/Http/Controllers/ServNacimientoController.php
@@ -619,7 +619,7 @@ class ServNacimientoController extends Controller
                 'cui_padre' => $cuiPadre,
                 'cui_madre' => $cuiMadre,
                 'id_muni' => $municipio,
-                'fecha' => $fechaNacimiento,
+                'fecha' => date("Y-m-d H:i:s", $fechaNacimiento),
                 'direccion_nac' => "ciudad",
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
