ca788c1e5559026bd13f0fb568128a6647bceaac
GaryGiron <gsteph393@gmail.com>
2019-01-07 22:48:01 -0600

    cambios en las vistas

--- resources/views/defuncion/resultado.blade.php
+++ resources/views/defuncion/resultado.blade.php
@@ -4,7 +4,7 @@
     <div class="panel panel-default">
         <div class="panel-body">
 
-            <b>dfsf: </b>{{$respData['message']}}            
+            <b>dfsf: </b>{{$respData}}            
         </div>
     </div>
 </div>
