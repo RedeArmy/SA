77de1aef422afc881d2d00dfb3f8c8507f591e44
GaryGiron <gsteph393@gmail.com>
2019-01-07 22:16:14 -0600

    cambios en las vistas

--- resources/views/defuncion/resultado.blade.php
+++ resources/views/defuncion/resultado.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.admin')
 <div class="container">
     <h1>Datos del fallecido</h1>
+    @foreach($respData as $data)
     <div class="panel panel-default">
         <div class="panel-body">
-            <b>cui: </b> {{$respData}}
+            <b>dfsf: </b>{{$data}}            
         </div>
     </div>
-    
+    @endforeach
 </div>
