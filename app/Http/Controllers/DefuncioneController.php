f214c0460f8a33382e8a86debc0470052290ade2
GaryGiron <gsteph393@gmail.com>
2019-01-07 22:01:42 -0600

    cambios en las vistas

--- app/Http/Controllers/DefuncioneController.php
+++ app/Http/Controllers/DefuncioneController.php
@@ -63,7 +63,7 @@ class DefuncioneController extends Controller
                 $err="cURL Error #:" . $err;
                 return view('defuncion.error', compact('err'));
             } else {
-                $respData = json_decode($response);
+                $respData = json_decode($response)->data;
                 return view('defuncion.resultado',compact('respData'));
             }
                         
