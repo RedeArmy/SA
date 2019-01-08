65d67b4274f1c5c39738dea69500bf70a9f2136e
GaryGiron <gsteph393@gmail.com>
2019-01-07 23:06:27 -0600

    cambios en las vistas

--- resources/views/defuncion/resultado.blade.php
+++ resources/views/defuncion/resultado.blade.php
@@ -3,8 +3,52 @@
     <h1>Datos del fallecido</h1>
     <div class="panel panel-default">
         <div class="panel-body">
-
-            <b>dfsf: </b>{{$info['cui']}}            
+            <b>cui: </b>{{$info['cui']]}}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+        <b>nombre: </b>{{$info['nombre']}}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+        <b>apellido: </b>{{$info['apellido']}}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+        <b>genero: </b>{{$info['genero']}}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+        <b>Fecha de Nacimiento: </b>{{$$info['fechaNacimiento']}}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+        <b>Pais: </b>{{$info['pais']}}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+        <b>Departamento: </b>{{$info['departamento']}}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+        <b>Municipio: </b>{{$info['municipio']}}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+        <b>Lugar de Nacimiento: </b>{{$info['lugarNacimiento']}}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+        <b>Estado Civil: </b>{{$info['estadoCivil']}}
         </div>
     </div>
 </div>
